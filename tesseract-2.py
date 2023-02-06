e9346b482a004d77ccf96f28013a17137f090267
Mohammad Ayaan Hashim <s2061408@ed.ac.uk>
2023-02-06 16:16:34 +0000

    fixed stuff

--- tesseract-2.py
+++ tesseract-2.py
@@ -3,9 +3,15 @@ import cv2
 
 pytesseract.pytesseract.tesseract_cmd = 'C:\Program Files\Tesseract-OCR\\tesseract.exe'
 
-img = cv2.imread('sample3.jpg')
 
-# img = cv2.resize(img, (600, 360))
-print(pytesseract.image_to_string(img))
-# cv2.imshow('Result', img)
-cv2.waitKey(0)
+def main(filepath):
+    img = cv2.imread(filepath)
+    # img = cv2.resize(img, (600, 360))
+    print(pytesseract.image_to_string(img))
+    # cv2.imshow('Result', img)
+    cv2.waitKey(0)
+
+
+if __name__ == "__main__":
+    filePath = 'sample3.jpg'
+    main(filePath)
